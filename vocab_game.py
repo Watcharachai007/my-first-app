18b8bb2c867a659993f827de97d523435142bf07
406-40-watcharachai <54798@yupparaj.ac.th>
2026-08-28 10:47:14 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -48,14 +48,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-    if u_ans3 == "📚 books":
+    if u_ans3 == "books":
        st.success("✅ ข้อ 3: ถูกต้อง")
        score += 1
     else:
       st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
       score += 1
 
-    if u_ans3 == "🥥 coconut":
+    if u_ans3 == "coconut":
        st.success("✅ ข้อ 4: ถูกต้อง")
        score += 1
     else:  
